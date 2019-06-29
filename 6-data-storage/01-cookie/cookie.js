603d84d252af607cab914d2385166619c6d1777d
xcurveballx <x.curveball.x@gmail.com>
2019-06-29 12:48:54 +0300

    cookie.js

--- 6-data-storage/01-cookie/cookie.js
+++ 6-data-storage/01-cookie/cookie.js
@@ -9,7 +9,7 @@ function setCookie(name, value, options = {}) {
 
   options = {
     path: '/',
-    // add other defaults here if necessary
+    // в случае необходимости можно добавить другие настройки по умолчанию
     ...options
   };
 
