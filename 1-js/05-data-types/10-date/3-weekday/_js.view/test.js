044633e0c65d41e2a05aa85962947a40b0ff09ee
pimonenkov <vadim.pimonenkov@yandex.com>
2019-06-30 10:28:26 +0300

    polish the article + work on tasks1..3

--- 1-js/05-data-types/10-date/3-weekday/_js.view/test.js
+++ 1-js/05-data-types/10-date/3-weekday/_js.view/test.js
@@ -1,29 +1,29 @@
-describe("getLocalDay returns the \"european\" weekday", function() {
-  it("3 January 2014 - friday", function() {
+describe("getLocalDay возвращает \"европейский\" деньнедели", function() {
+  it("3 января 2014 года - пятница", function() {
     assert.equal(getLocalDay(new Date(2014, 0, 3)), 5);
   });
 
-  it("4 January 2014 - saturday", function() {
+  it("4 января 2014 года - суббота", function() {
     assert.equal(getLocalDay(new Date(2014, 0, 4)), 6);
   });
 
-  it("5 January 2014 - sunday", function() {
+  it("5 января 2014 года - воскресенье", function() {
     assert.equal(getLocalDay(new Date(2014, 0, 5)), 7);
   });
 
-  it("6 January 2014 - monday", function() {
+  it("6 января 2014 года - понедельник", function() {
     assert.equal(getLocalDay(new Date(2014, 0, 6)), 1);
   });
 
-  it("7 January 2014 - tuesday", function() {
+  it("7 января 2014 года - вторник", function() {
     assert.equal(getLocalDay(new Date(2014, 0, 7)), 2);
   });
 
-  it("8 January 2014 - wednesday", function() {
+  it("8 января 2014 года - среда", function() {
     assert.equal(getLocalDay(new Date(2014, 0, 8)), 3);
   });
 
-  it("9 January 2014 - thursday", function() {
+  it("9 января 2014 года - четверг", function() {
     assert.equal(getLocalDay(new Date(2014, 0, 9)), 4);
   });
 });
