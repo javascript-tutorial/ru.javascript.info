9b870fc140040f1e4b15907893e9fcc50de34b1d
Daniel <58544605+letUser@users.noreply.github.com>
2020-02-10 10:49:11 +0300

    Delete solution.js

--- 1-js/05-data-types/05-array-methods/2-filter-range/_js.view/solution.js
+++
@@ -1,5 +0,0 @@
-
-function filterRange(arr, a, b) {
-  // добавлены скобки вокруг выражения для лучшей читаемости
-  return arr.filter(item => (a <= item && item <= b) || (a >= item && item >= b));
-}
