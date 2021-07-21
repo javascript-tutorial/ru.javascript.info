953acd38f6a861f523da5ada5176736977077bcb
ilyamed <0ilya@bk.ru>
2021-07-21 19:04:30 +0300

    Update solution.js
    Более компактный вид

--- 1-js/05-data-types/11-date/3-weekday/_js.view/solution.js
+++ 1-js/05-data-types/11-date/3-weekday/_js.view/solution.js
@@ -1,10 +1,3 @@
 function getLocalDay(date) {
-
-  let day = date.getDay();
-
-  if (day == 0) { // день недели 0 (воскресенье) в европейской нумерации будет 7
-    day = 7;
-  }
-
-  return day;
+  return (date.getDay()===0) ? 7 : date.getDay() // день недели 0 (воскресенье) в европейской нумерации будет 7
 }
