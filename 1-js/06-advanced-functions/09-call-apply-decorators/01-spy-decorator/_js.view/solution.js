eac6c7b329121a1e589b29fb826a6c5715d72ccf
Lavrentiy Rubtsov <rnbsov@gmail.com>
2022-08-26 20:50:21 +0600

    Update 1-js/06-advanced-functions/09-call-apply-decorators/01-spy-decorator/_js.view/solution.js
    Co-authored-by: Alexandre888 <alex30955@aol.com>

--- 1-js/06-advanced-functions/09-call-apply-decorators/01-spy-decorator/_js.view/solution.js
+++ 1-js/06-advanced-functions/09-call-apply-decorators/01-spy-decorator/_js.view/solution.js
@@ -1,7 +1,7 @@
 function spy(func) {
 
   function wrapper(...args) {
-    // мы используем ...args вместо аргументов для хранения "реального" массива в wrapper.calls
+    // мы используем ...args вместо arguments для хранения "реального" массива в wrapper.calls
     wrapper.calls.push(args);
     return func.apply(this, args);
   }
